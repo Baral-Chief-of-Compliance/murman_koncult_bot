03aaca0d128b6a0bc7d9e5ec8b384956f91d402b
unknown <dartmol21@mail.ru>
2026-01-23 19:53:14 +0300

    add new settings to work bot with proxy and add new info to deploy ts_bot on maschine without access to docker repositorys and npm and pip repositories

--- ts_bot/proxy.ts
+++ ts_bot/proxy.ts
@@ -5,6 +5,24 @@ import dotenv from 'dotenv'
 // настройка перменных виртуального окружения
 dotenv.config({ path: '.env'})
 
+interface ProxyInfo {
+    uri: string;
+    requestTls: {
+        rejectUnauthorized: boolean;
+    };
+    token?: string; // Необязательное свойство
+}
+
+let proxyInfo : ProxyInfo = {   
+        uri: `${process.env.PROXY_PATH}`,
+        requestTls: {
+            rejectUnauthorized: false,
+        },
+};
+
+if (process.env.PROXY_AUTH==='1'){
+    proxyInfo.token = `Basic ${Buffer.from(`${process.env.PROXY_LOGIN}:${process.env.PROXY_PASSWORD}`).toString('base64')}`
+}
 //Настройка прокси
 export const proxyAgent = new ProxyAgent(
     {   
